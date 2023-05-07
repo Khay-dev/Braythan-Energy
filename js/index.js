6d65e1c5b032203e3eb0b26d8b7f260592614ee7
Kalu Daniel <krayzhee419@gmail.com>
2023-05-07 21:55:16 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+const dropDownH = document.querySelector('.nav-menu')
+const spann = document.querySelector('.nav-svg')
+const bars = document.querySelectorAll('.bar')
+
+
+
+let bar2 = Array.from(bars)
+let toggleHamburger = () => {
+    bar2.forEach(bar => bar.classList.add('x'))
+}
+let removeHamburger = () => {
+    bar2.forEach(bar => bar.classList.remove("x"))
+}
+
+spann.addEventListener('click', () => {
+    if (dropDownH.style.display === "none") {
+        toggleHamburger()
+        dropDownH.style.display = "block";
+    } else {
+        removeHamburger()
+        dropDownH.style.display = "none";
+    }
+
+});
+
+
+
+
+
+
+
+
+
+function changeBg() {
+    let myNav = document.getElementById("nav");
+    let scrollValue = window.scrollY
+    if (scrollValue < 200) {
+        myNav.classList.remove('scroll')
+    } else {
+        myNav.classList.add('scroll')
+    }
+}
+
+
+window.addEventListener("scroll", changeBg)
+
